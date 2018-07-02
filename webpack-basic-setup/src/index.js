c09eccd54ef9af89e04afb13f98fbae910a9eb82
deinde dawodu <dopevilled@hotmail.com>
2018-07-02 01:01:20 -0400

    adding it all

--- webpack-basic-setup/src/index.js
+++ webpack-basic-setup/src/index.js
@@ -1,2 +1,8 @@
-import "./style.css";
+import "./scss/main.scss";
+import "./pageTwo.html";
+import "jquery";
 console.log("Hello, world!");
+$('.btnPro').click(function(){
+ $('.navRow  > ul').toggleClass('open')
+// alert('jque')
+})
--- webpack-basic-setup/webpack.config.js
+++ webpack-basic-setup/webpack.config.js
@@ -4,12 +4,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackMd5Hash = require("webpack-md5-hash");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-
+const webpack = require('webpack');
 module.exports = {
   entry: { main: "./src/index.js" },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].[chunkhash].js"
+    // filename: "[name].[chunkhash].js"
+    filename:"bundle.js"
   },
   module: {
     rules: [
@@ -21,27 +22,67 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
+        // test: /\.css$/
+        test:/\.(css|scss|css)$/,
         use: [
           "style-loader",
           MiniCssExtractPlugin.loader,
           "css-loader",
-          "postcss-loader"
+          "postcss-loader",
+          "sass-loader",
         ]
-      }
+      },   {
+        test:/\.html$/,
+        use:['html-loader']
+     },   
+      {
+      test:/\.(jpg|png)$/,
+      use:[
+          {
+              loader:'file-loader',
+              options:{
+                  name:'[name].[ext]',
+                  outputPath:'img/',
+                  publicPath:'img/'
+              }
+            }
+      ]
+  },
+      {
+        test:/\.html$/,
+        use:[
+            {
+                loader:'file-loader',
+                options:{
+                    name:'[name].[ext]'
+                }
+              }
+        ],
+        exclude: path.resolve(__dirname,'src/index.html')
+        
+    }
+
+    
     ]
   },
   plugins: [
     new CleanWebpackPlugin("dist", {}),
     new MiniCssExtractPlugin({
-      filename: "style.[contenthash].css"
+      // filename: "style.[contenthash].css"
+      filename: "[name]-styles.css",
+    chunkFilename: "[id].css"
     }),
     new HtmlWebpackPlugin({
-      inject: false,
-      hash: true,
+      // inject:true,
+      //  hash: true, for some reason this is causing css not to be injected keep commented out!!!
       template: "./src/index.html",
       filename: "index.html"
+      
     }),
-    new WebpackMd5Hash()
+    new webpack.ProvidePlugin({
+      $:'jquery', 
+      jQuery:'jquery'
+    })
+    // new WebpackMd5Hash()
   ]
 };
