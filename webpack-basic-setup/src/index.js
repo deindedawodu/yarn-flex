f5c73be01cc510eef918cfcff9fb05d6969a4ec8
deinde dawodu <dopevilled@hotmail.com>
2018-07-02 02:15:40 -0400

    new test from pull

--- webpack-basic-setup/src/index.js
+++ webpack-basic-setup/src/index.js
@@ -1,7 +1,7 @@
 import "./scss/main.scss";
 import "./pageTwo.html";
 import "jquery";
-console.log("Hello, world!");
+console.log("Hello, Yikes!");
 $('.btnPro').click(function(){
  $('.navRow  > ul').toggleClass('open')
 // alert('jque')
